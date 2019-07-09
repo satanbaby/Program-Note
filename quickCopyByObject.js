8348e8cd1214d572ecc94c7f72bfae27990b53e0
satanbaby <30554080+satanbaby@users.noreply.github.com>
2019-07-09 22:39:58 +0800

    在 quickCopyByObject 新增 axios 實例

--- quickCopyByObject.js
+++ quickCopyByObject.js
@@ -8,3 +8,23 @@ var http ={
 
 // 原始物件名稱:新的別名
 var {request:req, statusCode:status} = http
+
+// babel compiler:
+var http ={
+  request:'get',
+  response:'',
+  statusCode:200,
+  message:'success'
+};
+var req = http.request,
+ status = http.statusCode
+
+
+
+// 實用情景
+const url = `https://raw.githubusercontent.com/BensonRUEI/CSUMIS/master/gallery.json`;
+
+(async function(){
+  var {response: data} = await axios.get(url)  
+  console.log('data', data)
+})()
