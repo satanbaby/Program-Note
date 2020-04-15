bcc2fae9719557376b66031e854b6780e5b9b203
satanbaby <30554080+satanbaby@users.noreply.github.com>
2020-04-16 01:03:28 +0800

    Select column

--- linqSnippets.cs
+++ linqSnippets.cs
@@ -6,3 +6,9 @@ select item.Take(5)
 FackData
    .GroupBy (f => f.Gender)
    .Select (item => item.Take (5))
+// Select column
+FackData.GroupBy(_=>_.Gender, (key, enumerable)=> new 
+{
+	key = key,
+	First_Name = enumerable.Select(i=>i.First_name).Take(10)
+})
